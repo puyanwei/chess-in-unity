eb94ae105b000d2060dba16eab7f4346cb18731e
obarker <o.j.barker@hotmail.co.uk>
2021-05-09 20:59:52 +0100

    Logic added to identify the square any individual piece is on. Gizmos added for debugging.

--- Chess/Assets/Scripts/ArrayChess/PieceMover.cs
+++ Chess/Assets/Scripts/ArrayChess/PieceMover.cs
@@ -4,47 +4,62 @@ using UnityEngine;
 
 public class PieceMover : MonoBehaviour
 {
-    public Color hoverColor;
+    public Color squareHoverColor;
+    private Color squareStartColor;
+    public Color pieceHoverColor;
+    private Color pieceStartColor;
+    public Color nearSquare;
+
     private Renderer rend;
-    private Color startColor;
+    public Renderer squareRend;
+    
     private bool isHover = false;
     private bool boardArrayLoaded = false;
     public GameObject piece;
     public GameObject chessSquare;
-    private GameObject nearestSquare;
+    public GameObject nearestSquare;
     public GameObject[] chessSquares;
     public Vector3[] chessSquaresDistance;
+    private float oldDistance = 9999;
+    
 
 
-       
-
+    void squareColor() // Chessboard square spawner which allows for dynamic naming.
+    {
+        Renderer squareRend = piece.GetComponent("Renderer") as Renderer;
+    }
 
     void Start()
     {
        rend = GetComponent<Renderer>();
-        startColor = rend.material.color;
-
-
+        pieceStartColor = rend.material.color;      
     }
 
     void Update()
     {
         if (isHover == true && boardArrayLoaded == false)
         {
-            Debug.Log("Finding All Squares");
-            findAllSquares();
+            //squareColor();
+            //squareStartColor = squareRend.material.color;
+            findAllSquares();                 
             boardArrayLoaded = true;
         }
 
         if (isHover == true && boardArrayLoaded == true)
         {
-            foreach (GameObject chessSquare in chessSquares)
+            foreach (GameObject targetSquare in chessSquares)
             {
-                Debug.DrawLine(chessSquare.transform.position, piece.transform.position, Color.red, 0f, false);
+                Debug.DrawLine(targetSquare.transform.position, piece.transform.position, Color.red, 0f, false);
+                float dist = Vector3.Distance(this.gameObject.transform.position, targetSquare.transform.position);
+                if (dist < oldDistance)
+                {
+                    nearestSquare = targetSquare;
+                    oldDistance = dist;
+                }
+                //squareRend.material.color = squareHoverColor;
             }
                 
-        }
-        
+        }      
 
 
     }
@@ -52,23 +67,21 @@ public class PieceMover : MonoBehaviour
     void findAllSquares()
     {
         
-        chessSquares = GameObject.FindGameObjectsWithTag("square");
-
-        Debug.Log("Found All Squares");
+        chessSquares = GameObject.FindGameObjectsWithTag("square");     
         boardArrayLoaded = true;
     }
 
 
     void OnMouseOver()
     {
-        rend.material.color = hoverColor;
+        rend.material.color = pieceHoverColor;
         isHover = true;
 
     }
 
     void OnMouseExit()
     {
-        rend.material.color = startColor;
+        rend.material.color = pieceStartColor;
         isHover = false;
 
     }
