68b9aad1dbc9bd0f8846b0886f4bb41a7d4a75cd
obarker <o.j.barker@hotmail.co.uk>
2021-05-14 00:24:09 +0100

    i feel defeated as a human being. idea for cache system in comments.

--- Chess/Assets/Scripts/ArrayChess/PieceMover.cs
+++ Chess/Assets/Scripts/ArrayChess/PieceMover.cs
@@ -11,6 +11,8 @@ public class PieceMover : MonoBehaviour
     public Color pieceStartColor;
     public Color moveHoverColor;
     public Color moveStartColor;
+    public Color cacheColor;
+
 
     Ray ray;
     RaycastHit hit;
@@ -22,6 +24,7 @@ public class PieceMover : MonoBehaviour
     private Renderer rend;
     private Renderer pieceSquareRend;
     private Renderer moveSquareRend;
+    private Renderer parentSquareRend;
 
     private bool isHover = false;
     private bool isBoardArrayLoaded = false;
@@ -30,6 +33,7 @@ public class PieceMover : MonoBehaviour
     private bool isShowingMovement = false;
     private bool isClickedOnMoveSquare = false;
     private bool isInitialSquareRendererLoaded = false;
+    private bool isCached = false;
 
     public GameObject testCameraBody;
     public GameObject piece;
@@ -39,6 +43,7 @@ public class PieceMover : MonoBehaviour
     public GameObject[] pieces;
     public GameObject moveSquareParent;
     public GameObject moveSquare;
+    public GameObject cachedSquareObject;
 
     private string[] moveSquareName;
     public string nearestSquareParent;
@@ -48,7 +53,7 @@ public class PieceMover : MonoBehaviour
 
     private Vector3[] chessSquaresDistance;
 
-    private float targetDistance = 0.3f;
+    private float targetDistance = 1f;
     private float loadtimer = 1f;
 
     void Start()
@@ -57,6 +62,9 @@ public class PieceMover : MonoBehaviour
         pieceStartColor = rend.material.color;
 
     }
+
+
+
 
 
     void Update()
@@ -86,11 +94,14 @@ public class PieceMover : MonoBehaviour
 
         if (Input.GetMouseButton(0) && isSquareRendLoaded) // Clicking piece locks colour of movesquare until another piece is clicked.
         {
-
+            //cacheSquareData();
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Physics.Raycast(ray, out hit);
             rayHitStringParent = hit.transform.parent.name;
             getSquareRenderers();
+            isCached = true;
+            
+
 
             if (isShowingMovement && rayHitStringParent == moveSquare.transform.parent.name) // Move piece
             {
@@ -123,6 +134,44 @@ public class PieceMover : MonoBehaviour
         }
     }
 
+
+    // to do - write script to cache colour in array spawner per square. use getcomponent pull the cache color from square.
+
+    //void cacheSquareData()
+    //{
+    //    cachedSquareObject = GameObject.Find(nearestSquareParent);
+    //    parentSquareRend = cachedSquareObject.GetComponentInChildren<Renderer>();
+
+    //    Color blk = new Color(0, 0, 0, 1);
+    //    Color wht = new Color(255, 255, 255, 255);
+
+
+    //    if (isCached && cacheColor != blk || cacheColor != wht) // n piece selection or cache
+    //    {
+    //        cacheColor = parentSquareRend.material.color;
+    //        Debug.Log("second selection " + cacheColor);
+    //        Debug.Log(blk);
+    //        Debug.Log(wht);
+    //        Debug.Log("MouseDown " + Time.frameCount + " : ");
+
+
+    //    }
+
+    //    if (!isCached) // first selection or cache
+    //    {
+    //        cacheColor = parentSquareRend.material.color;
+    //        Debug.Log(cacheColor);
+
+
+    //        isCached = true;
+
+
+    //    }
+
+    //}
+
+
+
     void resetColors(bool updateColor)
     {
         pieceSquareRend.material.color = squareStartColor;
@@ -138,15 +187,18 @@ public class PieceMover : MonoBehaviour
         {
             float dist = Vector3.Distance(this.gameObject.transform.position, _targetSquare.transform.position);
             float debugsum = (targetDistance - dist);
+            
 
             if (dist < targetDistance)
             {
                 nearestSquare = _targetSquare;
                 nearestSquareParent = _targetSquare.gameObject.transform.parent.name;
+                //targetDistance = dist;
                 pieceSquareRend = nearestSquare.GetComponent<Renderer>() as Renderer;
                 squareStartColor = pieceSquareRend.material.color;
                 moveableSquares();
                 isSquareRendLoaded = true;
+                
             }
 
         }
